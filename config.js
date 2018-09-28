557e3e64492d0d794534ca42d58c112493b72389
Antony Carrizo <antonycarrizo96@gmail.com>
2018-09-28 09:59:01 -0400

    [UPD]: - Expuesto token secreto.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: process.env.PORT || 3000,
     URI_DB:  process.env.DATABASE_URL || 'postgres://admin:admin@localhost:5432/admin',
-    // SECRET_TOKEN: 'miclavedetokens'
+    SECRET_TOKEN: 'miclavedetokens'
 };
 export default config;
