d7847c87f61a979e3e815f956a2999a5eccf3a1b
Antony Carrizo <antonycarrizo96@gmail.com>
2018-10-03 09:28:09 -0400

    [UPD]: - Creada ruta para mostrar todos los routes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import auth from 'middlewares/auth';
 import userCtrl from 'controllers/user';
+import { User } from 'database/models';
 const api = express.Router();
 
 api.post('/signup', userCtrl.signUp);
 api.post('/signin', userCtrl.singIn);
+api.get('/private', auth, (req, res) => {
+  res.status(200).send({ message: 'Tienes acceso' });
+});
+api.get('/users', async (req, res) => {
+  let users = await User.findAll();
+  res.send({ users: users });
+});
 
 module.exports = api;
